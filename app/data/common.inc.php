69ceccbb84789279475d2bc8c014a9e3da93bed9
Peter Zhang <peter@cervinmedia.co.nz>
2021-01-27 22:05:32 +1300

    first commit

---
+++ app/data/common.inc.php
@@ -0,0 +1,10 @@
+<?php 
+//数据库连接信息
+$cfg_dbhost = 'db';
+$cfg_dbport = 3306;
+$cfg_dbname = 'movie';
+$cfg_dbuser = 'movie';
+$cfg_dbpwd = 'movie';
+$cfg_dbprefix = 'sea_';
+$cfg_db_language = 'utf8';
+?>
